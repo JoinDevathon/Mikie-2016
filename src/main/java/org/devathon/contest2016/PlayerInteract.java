7e340ef3625b8147dd80144bc9aed467d863df6b
Michael <scebelmj@yahoo.com>
2016-11-05 13:39:13 -0400

    Working still

--- src/main/java/org/devathon/contest2016/PlayerInteract.java
+++ src/main/java/org/devathon/contest2016/PlayerInteract.java
@@ -1,13 +1,13 @@
 package org.devathon.contest2016;
 
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Guardian;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by Michael on 11/5/2016.
@@ -22,12 +22,16 @@ public class PlayerInteract implements Listener {
         if(e.getAction() == Action.LEFT_CLICK_AIR || !e.getPlayer().getItemInHand().getType().equals(Material.BLAZE_ROD)){
             e.setCancelled(true);
         }
-
-
         Guardian g = (Guardian) p.getWorld().spawnEntity(p.getLocation(), EntityType.GUARDIAN);
+        Bat b = (Bat) p.getWorld().spawnEntity(p.getLocation(), EntityType.BAT);
+        ArmorStand a = (ArmorStand) p.getWorld().spawnEntity(p.getLocation(), EntityType.ARMOR_STAND);
+        a.setVisible(true);
+        b.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1, true, false));
+        b.setPassenger(a);
+        b.setGravity(true);
+        a.setGravity(false);
+        a.setPassenger(g);
         g.setGravity(false);
-        g.setGlowing(false);
-        g.getLeashHolder().equals(Material.BLAZE_ROD);
         p.sendMessage("I hope this works");
 
     }
